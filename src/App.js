3b47f37c97e83c7286ca25aa3cede36d73053d4b
Luis Anibal Zapeta de leon <72290798+Anibal89@users.noreply.github.com>
2022-09-12 22:15:09 -0600

    doc
    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Api from './components/api';
+
+function App() {
+  return (
+    
+
+    <Api></Api>
+
+  );
+}
+
+export default App;
---
+++ src/components/api.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from "react";
+
+const Api = () =>{
+
+
+    const[equipo ,setEquipo] = useState([])
+
+useEffect (() => {
+
+   //console.log('hola')
+  obtenerDatos()
+
+},[])
+
+const obtenerDatos = async() => {
+const data = await fetch('https://jsonplaceholder.typicode.com/users')
+const users = await data.json()
+ //console.log(users)
+setEquipo(users)
+}
+
+
+return(
+<div>
+<h1 style={{textAlign:'center'}}>Prueba de hooks :v</h1>
+<ul>
+    {
+        equipo.map(item => (
+            <li key={item.id}>{item.name} - {item.email}</li>
+
+        ))
+    }
+</ul>
+</div>
+);
+
+
+
+}
+
+export default Api
