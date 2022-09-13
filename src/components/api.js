1bc18ad48a655022e61e5ae52e52ed49ec2b10a1
Anibal89 <72290798+Anibal89@users.noreply.github.com>
2022-09-13 07:33:08 -0600

    update api
    eliminar

--- src/components/api.js
+++ src/components/api.js
@@ -1,42 +1,33 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-const Api = () =>{
+const Api = () => {
+  const [equipo, setEquipo] = useState([]);
 
+  useEffect(() => {
+    //console.log('hola')
+    obtenerDatos();
+  }, []);
 
-    const[equipo ,setEquipo] = useState([])
+  const obtenerDatos = async () => {
+    const data = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await data.json();
+    //console.log(users)
+    setEquipo(users);
+  };
 
-useEffect (() => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Prueba de hooks :v</h1>
+      <ul>
+        {equipo.map((item) => (
+          <li key={item.id}>
+            {" "}
+            {item.name} - {item.email} - {item.phone}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-    
-   //console.log('hola')
-  obtenerDatos()
-
-},[])
-
-const obtenerDatos = async() => {
-const data = await fetch('https://jsonplaceholder.typicode.com/users')
-const users = await data.json()
- //console.log(users)
-setEquipo(users)
-}
-
-
-return(
-<div>
-<h1 style={{textAlign:'center'}}>Prueba de hooks :v</h1>
-<ul>
-    {
-        equipo.map(item => (
-            <li key={item.id}> {item.name} - {item.email} - {item.phone}</li>
-
-        ))
-    }
-</ul>
-</div>
-);
-
-
-
-}
-
-export default Api
+export default Api;
